8a70e570e7ae211bed42844bc58f6f4c23e97ba9
Zacharia Schmitz <schmitz.zacharia@gmail.com>
2023-08-11 12:50:41 -0500

    Exercise post-review

--- acquire.py
+++ acquire.py
@@ -2,24 +2,25 @@ from env import db_url
 import pandas as pd
 import os
 
+import pandas as pd
 
-def grab_sql(db, table, filename, query=None):
+
+def grab_sql(db, query, filename):
     """
-    grab sql will grab data from codeups mysql server utilizing db_url
-    with credentials from a personal env file. It will check if filename file exists.
-    If it does not exist, it will create one.
+    Grab data from Codeup's MySQL server utilizing db_url with credentials from a personal env file.
+    Check if filename file exists. If it does not exist, it will create one.
 
-    This will only select ALL data from a table with no limit on size.
+    This function selects ALL data from a table with no limit on size.
 
-    arguments: db is the database
-               table is the table within that database
-               filename is what the file will be named
-               query is the SQL query to execute (default is "SELECT * FROM <table>")
+    Args:
+        db (str): The name of the database.
+        table (str): The name of the table within that database.
+        filename (str): The name of the file to be created.
+        query (str, optional): The SQL query to execute. Defaults to "SELECT * FROM <table>".
 
-    return: a pandas dataframe
+    Returns:
+        pandas.DataFrame: A pandas dataframe containing the data from the selected table.
     """
-    if query is None:
-        query = f"SELECT * FROM {table};"
     connection = db_url(db)
     df = pd.read_sql(query, connection)
     df.to_csv(filename, index=False)
@@ -32,26 +33,12 @@ def grab_sql(db, table, filename, query=None):
 
 def get_titanic_data():
     """
-    get titanic data will query the titanic database and return all the data within
-
-    arguments: none
-
-    return: a pandas dataframe
+    This function retrieves the titanic data from the 'passengers' table in the 'titanic_db' database and returns it as a pandas DataFrame.
     """
-    filename = "titanic.csv"
-    if os.path.isfile(filename):
-        df = pd.read_csv(filename)
-    else:
-        query = "SELECT * FROM passengers"
-        connection = db_url("titanic_db")
-        df = pd.read_sql(query, connection)
-        df.to_csv(filename, index=False)
+    query = "select * from passengers"
+    df = grab_sql("titanic_db", query, "titanic.csv")
     return df
 
-
-# def get_titanic_data():
-#     grab_data('titanic_db', 'passengers')
-#     return df
 
 # 2. Make a function named get_iris_data that returns the data from the iris_db on the codeup data science database as a pandas data frame.
 # The returned data frame should include the actual name of the species in addition to the species_ids.
@@ -60,24 +47,18 @@ def get_titanic_data():
 
 def get_iris_data():
     """
-    get iris will query the iris database and return all the data within
+    This function retrieves the iris dataset from a SQL database and returns it as a pandas DataFrame.
 
-    arguments: none
-
-    return: a pandas dataframe
+    Returns:
+    - df (pandas DataFrame): The iris dataset with species names and measurements.
     """
-    filename = "iris.csv"
-    if os.path.isfile(filename):
-        df = pd.read_csv(filename)
-    else:
-        query = """
-        SELECT DISTINCT measurements.*, species.species_name
+    query = """
+        SELECT species.species_name, measurements.*
         FROM measurements
         JOIN species
         ON measurements.species_id = species.species_id;"""
-        connection = db_url("iris_db")
-        df = pd.read_sql(query, connection)
-        df.to_csv(filename, index=False)
+
+    df = grab_sql("iris_db", query, "iris.csv")
     return df
 
 
@@ -89,17 +70,12 @@ def get_iris_data():
 
 def get_telco_data():
     """
-    get telco data will query the telco database and return all the relevant churn data within
+    This function retrieves data from the 'customers' table in the 'telco_churn' database, and joins it with the 'contract_types', 'internet_service_types', and 'payment_types' tables. The resulting dataframe contains information about customers' contracts, internet service types, payment types, and other relevant information.
 
-    arguments: none
-
-    return: a pandas dataframe
+    Returns:
+    - df: a pandas DataFrame containing the joined data from the 'customers', 'contract_types', 'internet_service_types', and 'payment_types' tables.
     """
-    filename = "telco.csv"
-    if os.path.isfile(filename):
-        df = pd.read_csv(filename)
-    else:
-        query = """
+    query = """
         SELECT *
         FROM customers
         JOIN contract_types
@@ -108,9 +84,8 @@ def get_telco_data():
         ON customers.internet_service_type_id = internet_service_types.internet_service_type_id
         JOIN payment_types
         ON customers.payment_type_id = payment_types.payment_type_id;"""
-        connection = db_url("telco_churn")
-        df = pd.read_sql(query, connection)
-        df.to_csv(filename, index=False)
+
+    df = grab_sql("telco_churn", query, "telco.csv")
     return df
 
 
@@ -120,4 +95,70 @@ def get_telco_data():
 # then write the dataframe to a .csv file with the appropriate name.
 
 
-###
+##### OLD CODE prior to 'grab_sql()' consolidation
+
+# def get_telco_data():
+#     """
+#     get telco data will query the telco database and return all the relevant churn data within
+
+#     arguments: none
+
+#     return: a pandas dataframe
+#     """
+#     filename = "telco.csv"
+#     if os.path.isfile(filename):
+#         df = pd.read_csv(filename)
+#     else:
+#         query = """
+#         SELECT *
+#         FROM customers
+#         JOIN contract_types
+#         ON customers.contract_type_id = contract_types.contract_type_id
+#         JOIN internet_service_types
+#         ON customers.internet_service_type_id = internet_service_types.internet_service_type_id
+#         JOIN payment_types
+#         ON customers.payment_type_id = payment_types.payment_type_id;"""
+#         connection = db_url("telco_churn")
+#         df = pd.read_sql(query, connection)
+#         df.to_csv(filename, index=False)
+#     return df
+
+# def get_iris_data():
+#     """
+#     get iris will query the iris database and return all the data within
+
+#     arguments: none
+
+#     return: a pandas dataframe
+#     """
+#     filename = "iris.csv"
+#     if os.path.isfile(filename):
+#         df = pd.read_csv(filename)
+#     else:
+#         query = """
+#         SELECT species.species_name, measurements.*
+#         FROM measurements
+#         JOIN species
+#         ON measurements.species_id = species.species_id;"""
+#         connection = db_url("iris_db")
+#         df = pd.read_sql(query, connection)
+#         df.to_csv(filename, index=False)
+#     return df
+
+# def get_titanic_data():
+#     """
+#     get titanic data will query the titanic database and return all the data within
+
+#     arguments: none
+
+#     return: a pandas dataframe
+#     """
+#     filename = "titanic.csv"
+#     if os.path.isfile(filename):
+#         df = pd.read_csv(filename)
+#     else:
+#         query = "SELECT * FROM passengers"
+#         connection = db_url("titanic_db")
+#         df = pd.read_sql(query, connection)
+#         df.to_csv(filename, index=False)
+#     return df
