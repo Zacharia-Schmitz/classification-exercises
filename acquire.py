ce2d38eb250b48988680108a70c65259e852b1d8
Zacharia Schmitz <schmitz.zacharia@gmail.com>
2023-08-17 17:22:33 -0500

    Exercise post-review

--- acquire.py
+++ acquire.py
@@ -31,13 +31,13 @@ def grab_sql(db, query, filename):
 # Obtain your data from the Codeup Data Science Database.
 
 
-def get_titanic_data():
-    """
-    This function retrieves the titanic data from the 'passengers' table in the 'titanic_db' database and returns it as a pandas DataFrame.
-    """
-    query = "select * from passengers"
-    df = grab_sql("titanic_db", query, "titanic.csv")
-    return df
+# def get_titanic_data():
+#     """
+#     This function retrieves the titanic data from the 'passengers' table in the 'titanic_db' database and returns it as a pandas DataFrame.
+#     """
+#     query = "select * from passengers"
+#     df = grab_sql("titanic_db", query, "titanic.csv")
+#     return df
 
 
 # 2. Make a function named get_iris_data that returns the data from the iris_db on the codeup data science database as a pandas data frame.
@@ -45,21 +45,21 @@ def get_titanic_data():
 # Obtain your data from the Codeup Data Science Database.
 
 
-def get_iris_data():
-    """
-    This function retrieves the iris dataset from a SQL database and returns it as a pandas DataFrame.
+# def get_iris_data():
+#     """
+#     This function retrieves the iris dataset from a SQL database and returns it as a pandas DataFrame.
 
-    Returns:
-    - df (pandas DataFrame): The iris dataset with species names and measurements.
-    """
-    query = """
-        SELECT species.species_name, measurements.*
-        FROM measurements
-        JOIN species
-        ON measurements.species_id = species.species_id;"""
+#     Returns:
+#     - df (pandas DataFrame): The iris dataset with species names and measurements.
+#     """
+#     query = """
+#         SELECT species.species_name, measurements.*
+#         FROM measurements
+#         JOIN species
+#         ON measurements.species_id = species.species_id;"""
 
-    df = grab_sql("iris_db", query, "iris.csv")
-    return df
+#     df = grab_sql("iris_db", query, "iris.csv")
+#     return df
 
 
 # 3. Make a function named get_telco_data that returns the data from the telco_churn database in SQL.
@@ -68,48 +68,14 @@ def get_iris_data():
 # Obtain your data from the Codeup Data Science Database.
 
 
-def get_telco_data():
-    """
-    This function retrieves data from the 'customers' table in the 'telco_churn' database, and joins it with the 'contract_types', 'internet_service_types', and 'payment_types' tables. The resulting dataframe contains information about customers' contracts, internet service types, payment types, and other relevant information.
-
-    Returns:
-    - df: a pandas DataFrame containing the joined data from the 'customers', 'contract_types', 'internet_service_types', and 'payment_types' tables.
-    """
-    query = """
-        SELECT *
-        FROM customers
-        JOIN contract_types
-        ON customers.contract_type_id = contract_types.contract_type_id
-        JOIN internet_service_types
-        ON customers.internet_service_type_id = internet_service_types.internet_service_type_id
-        JOIN payment_types
-        ON customers.payment_type_id = payment_types.payment_type_id;"""
-
-    df = grab_sql("telco_churn", query, "telco.csv")
-    return df
-
-
-# 4. Once you've got your get_titanic_data, get_iris_data, and get_telco_data functions written, now it's time to add caching to them.
-# To do this, edit the beginning of the function to check for the local filename of telco.csv, titanic.csv, or iris.csv.
-# If they exist, use the .csv file. If the file doesn't exist, then produce the SQL and pandas necessary to create a dataframe,
-# then write the dataframe to a .csv file with the appropriate name.
-
-
-##### OLD CODE prior to 'grab_sql()' consolidation
-
 # def get_telco_data():
 #     """
-#     get telco data will query the telco database and return all the relevant churn data within
+#     This function retrieves data from the 'customers' table in the 'telco_churn' database, and joins it with the 'contract_types', 'internet_service_types', and 'payment_types' tables. The resulting dataframe contains information about customers' contracts, internet service types, payment types, and other relevant information.
 
-#     arguments: none
-
-#     return: a pandas dataframe
+#     Returns:
+#     - df: a pandas DataFrame containing the joined data from the 'customers', 'contract_types', 'internet_service_types', and 'payment_types' tables.
 #     """
-#     filename = "telco.csv"
-#     if os.path.isfile(filename):
-#         df = pd.read_csv(filename)
-#     else:
-#         query = """
+#     query = """
 #         SELECT *
 #         FROM customers
 #         JOIN contract_types
@@ -118,47 +84,81 @@ def get_telco_data():
 #         ON customers.internet_service_type_id = internet_service_types.internet_service_type_id
 #         JOIN payment_types
 #         ON customers.payment_type_id = payment_types.payment_type_id;"""
-#         connection = db_url("telco_churn")
-#         df = pd.read_sql(query, connection)
-#         df.to_csv(filename, index=False)
+
+#     df = grab_sql("telco_churn", query, "telco.csv")
 #     return df
 
-# def get_iris_data():
-#     """
-#     get iris will query the iris database and return all the data within
 
-#     arguments: none
+# 4. Once you've got your get_titanic_data, get_iris_data, and get_telco_data functions written, now it's time to add caching to them.
+# To do this, edit the beginning of the function to check for the local filename of telco.csv, titanic.csv, or iris.csv.
+# If they exist, use the .csv file. If the file doesn't exist, then produce the SQL and pandas necessary to create a dataframe,
+# then write the dataframe to a .csv file with the appropriate name.
 
-#     return: a pandas dataframe
-#     """
-#     filename = "iris.csv"
-#     if os.path.isfile(filename):
-#         df = pd.read_csv(filename)
-#     else:
-#         query = """
-#         SELECT species.species_name, measurements.*
-#         FROM measurements
-#         JOIN species
-#         ON measurements.species_id = species.species_id;"""
-#         connection = db_url("iris_db")
-#         df = pd.read_sql(query, connection)
-#         df.to_csv(filename, index=False)
-#     return df
 
-# def get_titanic_data():
-#     """
-#     get titanic data will query the titanic database and return all the data within
+def get_telco_data():
+    """
+    get telco data will query the telco database and return all the relevant churn data within
 
-#     arguments: none
+    arguments: none
 
-#     return: a pandas dataframe
-#     """
-#     filename = "titanic.csv"
-#     if os.path.isfile(filename):
-#         df = pd.read_csv(filename)
-#     else:
-#         query = "SELECT * FROM passengers"
-#         connection = db_url("titanic_db")
-#         df = pd.read_sql(query, connection)
-#         df.to_csv(filename, index=False)
-#     return df
+    return: a pandas dataframe
+    """
+    filename = "telco.csv"
+    if os.path.isfile(filename):
+        df = pd.read_csv(filename)
+    else:
+        query = """
+        SELECT *
+        FROM customers
+        JOIN contract_types
+        ON customers.contract_type_id = contract_types.contract_type_id
+        JOIN internet_service_types
+        ON customers.internet_service_type_id = internet_service_types.internet_service_type_id
+        JOIN payment_types
+        ON customers.payment_type_id = payment_types.payment_type_id;"""
+        connection = db_url("telco_churn")
+        df = pd.read_sql(query, connection)
+        df.to_csv(filename, index=False)
+    return df
+
+
+def get_iris_data():
+    """
+    get iris will query the iris database and return all the data within
+
+    arguments: none
+
+    return: a pandas dataframe
+    """
+    filename = "iris.csv"
+    if os.path.isfile(filename):
+        df = pd.read_csv(filename)
+    else:
+        query = """
+        SELECT species.species_name, measurements.*
+        FROM measurements
+        JOIN species
+        ON measurements.species_id = species.species_id;"""
+        connection = db_url("iris_db")
+        df = pd.read_sql(query, connection)
+        df.to_csv(filename, index=False)
+    return df
+
+
+def get_titanic_data():
+    """
+    get titanic data will query the titanic database and return all the data within
+
+    arguments: none
+
+    return: a pandas dataframe
+    """
+    filename = "titanic.csv"
+    if os.path.isfile(filename):
+        df = pd.read_csv(filename)
+    else:
+        query = "SELECT * FROM passengers"
+        connection = db_url("titanic_db")
+        df = pd.read_sql(query, connection)
+        df.to_csv(filename, index=False)
+    return df
